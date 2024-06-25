987e8bc4f18a8cc3898234ad03e5e74521161074
Luis Perez <lperezdev843@gmail.com>
2024-06-25 01:52:51 -0400

    app deployed and Auth0 configured

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BtFvHENH/OsZkhxFqsoaxw8DuCwRcaeEBx6dUyJrkoc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x03OLsaI6tM5ZTjnCOIBPu7yAmDk49iLywZYZxULlk0=\"\n}"
